5694ca125c9b7eec36fec70ae08ec990ca4513ee
BigdraCo1 <pakawat.tab@gmail.com>
2024-12-16 18:46:18 +0000

    TCPserver : while loop for real time chat

--- TCP/server/TCPserver.c
+++ TCP/server/TCPserver.c
@@ -2,11 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 
 #define PORT 9000
 #define BUFFER_SIZE 1024
+
+void error(char *msg) {
+  perror(msg);
+  exit(1);
+}
 
 int main() {
   int networkSocket;
@@ -30,12 +36,31 @@ int main() {
   bind(networkSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
   printf("[+]Bind to the port %d\n", PORT);
 
+  // Listen new socket
   listen(networkSocket, SOMAXCONN);
   printf("[+]Listening...\n");
   addr_size = sizeof(newAddr);
 
+  // Client socket
   newSocket = accept(networkSocket, (struct sockaddr *)&newAddr, &addr_size);
 
-  strcpy(buff, "Nigga\n");
-  send(newSocket, buff, strlen(buff), 0);
+  while (1) {
+    bzero(buff, BUFFER_SIZE);
+    n = read(newSocket, buff, BUFFER_SIZE);
+    if (n < 0)
+      error("Error on reading.");
+    printf("Client : %s\n", buff);
+
+    bzero(buff, BUFFER_SIZE);
+    fgets(buff, BUFFER_SIZE, stdin);
+    n = write(newSocket, buff, strlen(buff));
+    if (n < 0)
+      error("Error on writing.");
+    if (strcmp("Zhaijian", buff, 8) == 0)
+      break;
+  }
+
+  close(newSocket);
+  close(networkSocket);
+  return 0;
 }
