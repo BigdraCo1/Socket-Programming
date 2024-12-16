4f76fd21fa7165d4744af9f2b576ffe6726afd24
BigdraCo1 <pakawat.tab@gmail.com>
2024-12-16 08:19:07 +0000

    TCPServer : adjust file

--- TCP/server/TCPserver.c
+++ TCP/server/TCPserver.c
@@ -1,5 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-void main() {
-  printf("Hello world\n");
+#define PORT 9000
+#define BUFFER_SIZE 1024
+  
+int main() {
+  int networkSocket;
+  struct sockaddr_in serverAddr;
+
+  int newSocket;
+  struct sockaddr_in newAddr;
+
+  socklen_t addr_size;
+  char buff[BUFFER_SIZE];
+
+  networkSocket = socket(AF_INET, SOCK_STREAM, 0);
+  // initializes the struct to all zeros to ensure no garbage values
+  bzero(&serverAddr, sizeof(serverAddr));
+  serveraddr.sin_family = AF_INET;
+  serveraddr.sin_port = htons(PORT);
+  serveraddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+  // bind socket 
+  bind(networkSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+  
+  listen(networkSocket, SOMAXXCONN);
+  addr_size = sizeof(newAddr);
+
+  
 }
